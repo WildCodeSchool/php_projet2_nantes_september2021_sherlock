70eb32dc2c9cb4c52cdc6e55654713f04eedf344
jessie-durand <jessie_durand@hotmail.fr>
2021-11-12 15:03:42 +0100

    réajustement css pages admin

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -4,15 +4,15 @@ require_once "../View/sherlock/game.html.twig";
 session_start();
 
 //  Cookies pour limiter le nombre de questions à 3 
-    if(!isset($_SESSION['listeDeQuestions']) && array_count_values($_SESSION['questions']) > 3) : ?>
+    if(!isset($_SESSION['questions']) && array_count_values($_SESSION['question']) > 3) : ?>
         <div> Vous ne pouvez sélectionner que trois questions. </div>
     <?php else : ?>
 
 <!-- // Cookies pour stocker les réponses/questions choisis dans le container indices -->
-        <?php foreach(array_unique($_SESSION['questions']) as $question_id): ?>
+        <?php foreach(array_unique($_SESSION['question']) as $question_id): ?>
             <div class="texteindice">
                 <ul> 
-                    <li> <?= array_unique($_SESSION['listeDeReponses'] [$reponse_id]) ?> </li>
+                    <li> <?= array_unique($_SESSION['reponses'] [$reponse_id]) ?> </li>
                 </ul>
         <?php endforeach ?>
     <?php endif ?>   
