2327c682cebc5d2e1ce86e03fa60ed1e23f72c80
jessie-durand <jessie_durand@hotmail.fr>
2021-11-18 13:34:13 +0100

    conflits gérés

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -32,63 +32,11 @@ btnPopup3.addEventListener('click', e => {
     overlay3.style.display = 'block';
 });
 
-<<<<<<< HEAD
   
 var btnClose3 = document.getElementById('btnClose3');
   btnClose3.addEventListener('click',e=>{
     overlay3.style.display='none';
 });
-
-// Fenêtre pop-up carte moriarty affichant les indices dans texte indice sur la page Police
-var btnPopup4 = document.getElementById('btnPopup4');
-var overlay4 = document.getElementById('overlay4');
-btnPopup4.addEventListener('click',e=>{
-overlay4.style.display='block';
-});
-
-var btnClose4 = document.getElementById('btnClose4');
-btnClose4.addEventListener('click',e=>{
-    overlay4.style.display='none';
-});
-
-// Fenêtre pop-up carte Baker affichant les indices dans texte indice sur la page Police
-var overlay5 = document.getElementById('overlay5');
-btnPopup5.addEventListener('click',e=>{
-overlay5.style.display='block';
-});
-
-var btnClose5 = document.getElementById('btnClose5');
-btnClose5.addEventListener('click',e=>{
-    overlay5.style.display='none';
-});
-
-// Fenêtre pop-up carte James affichant les indices dans texte indice sur la page Police
-var btnPopup6 = document.getElementById('btnPopup6');
-var overlay6 = document.getElementById('overlay6');
-btnPopup6.addEventListener('click',e=>{
-overlay6.style.display='block';
-});
-
-var btnClose6 = document.getElementById('btnClose6');
-btnClose6.addEventListener('click',e=>{
-    overlay6.style.display='none';
-});
-=======
-var btnClose3 = document.getElementById('btnClose3');
-btnClose3.addEventListener('click', e => {
-    overlay3.style.display = 'none';
-});
-
-var btnClose9 = document.getElementById('btnClose9');
-btnClose9.addEventListener('click',e=>{
-    overlay9.style.display='none';
-});
-
-
-
->>>>>>> b1f9cd4d1a16a8b146c708e59d6acd0f010ac4f3
-
-
 
 //reponses
 function showContent(index, name) {
@@ -100,14 +48,4 @@ function showContent(index, name) {
     else {
         element.style.display = 'none';
     }
-<<<<<<< HEAD
-} 
-
-
-
-
-
-
-=======
 }
->>>>>>> b1f9cd4d1a16a8b146c708e59d6acd0f010ac4f3
